8a18d3b5b689a580d5334ba57bdb29b834591a7a
Ezra Chai <juanzhe2@gmail.com>
2024-10-23 12:31:22 +0800

    changed line

--- letters.cpp
+++ letters.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main(){
-
     char letter1 = 65;
     char letter2 = 66;
     char letter3 = 'C';
 
     cout << letter1 << letter2  << letter3;
-
+    
     return 0;
 }
